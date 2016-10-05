d706b4fe6e33798846d7dc89aba7c839a8ee07c4
Michael Lai <mike24567@gmail.com>
2016-10-05 15:34:40 -0400

    Finished Silver level submission

--- app.rb
+++ app.rb
@@ -1,11 +1,13 @@
 require "sinatra"
 require "sinatra/reloader"
 require "active_record"
-
+# require 'pry'
 
 require_relative "db/connection"
 require_relative "models/pokemon"
+require_relative "models/trainer"
 
+# Pokemon Information ++++++++++++++++++++++++++
 get "/" do
   @pokemon = Pokemon.all
   erb :"pokemon/index"
@@ -42,7 +44,51 @@ delete "/:id" do
 end
 
 
+
+# Trainer information ++++++++++++++++++++++++++
+get "/trainers" do
+  @trainers = Trainer.all
+  erb :"trainer/index"
+end
+
+get "/trainers/new" do
+  erb :"trainer/new"
+end
+
+post "/trainers" do
+  Trainer.create(params[:trainer])
+  redirect "/trainers"
+end
+
+get "/trainers/:id/edit" do
+  @trainer = Trainer.find(params[:id])
+  erb :"trainer/edit"
+end
+
+put "/trainers/:id" do
+  @trainer = Trainer.find(params[:id])
+  @trainer.update(params[:trainer])
+  redirect "/trainers"
+end
+
+delete "/trainers/:id" do
+  @trainer = Trainer.find(params[:id])
+  @trainer.destroy
+  redirect "/trainers"
+end
+
+get "/trainers/:id" do
+  @trainer = Trainer.find(params[:id])
+  @pokemons = @trainer.pokemons.map {|poke| poke.name}
+  erb :"trainer/show"
+end
+
+# KEEP THIS AT THE BOTTOM SO IT ID EVERYTHING
 get "/:id" do
   @pokemon = Pokemon.find(params[:id])
+  @trainer = @pokemon.trainer.name
   erb :"pokemon/show"
 end
+#
+# binding.pry
+# puts "done"
--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,18 @@
 DROP TABLE IF EXISTS pokemons;
+DROP TABLE IF EXISTS trainers;
 
 create table pokemons (
   id SERIAL PRIMARY KEY,
   name VARCHAR,
   poke_type VARCHAR,
   cp INTEGER,
+  img_url VARCHAR,
+  trainer_id INTEGER
+);
+
+create table trainers (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR,
+  level INTEGER,
   img_url VARCHAR
 );
